19a22fc141d83a1d8304e39611a90fcbcd3d2ef7
remiPra <remipradere@gmail.com>
2020-02-02 23:15:49 +0100

    chagement de la base de données connecté cette fois ci sur ionos

--- models/connect.php
+++ models/connect.php
@@ -1,7 +1,19 @@
 <?php
 
 
-    $bdd = new PDO('mysql:host=localhost;dbname=test;charset=utf8', 'root', '');
+    //$bdd = new PDO('mysql:host=localhost;dbname=test;charset=utf8', 'root', '');
+    
+    
+    $host_name = 'db5000267422.hosting-data.io';
+    $database = 'dbs260968';
+    $user_name = 'dbu246755';
+    $password = 'Tfctfc3131@';
+  
+  
+   
+      $bdd = new PDO("mysql:host=$host_name; dbname=$database;", $user_name, $password);
+   
+  
    
 
 
